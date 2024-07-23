152e697fe842532f837cb65b2f41986e435ffd4f
yuan-miranda <yuanezekielamiranda@gmail.com>
2024-07-23 22:32:21 +0800

    fixed the path error when browsing the archive recursively

--- files/webarchive/07232024_9eeaa41/main.js
+++ files/webarchive/07232024_9eeaa41/main.js
@@ -1,14 +1,13 @@
 const versions = [
-    "files/webarchive/04092024_30b04de_1/index.html",
-    "files/webarchive/04092024_ef4a586_2/index.html",
-    "files/webarchive/04102024_12c236c_1/index.html",
-    "files/webarchive/04102024_374ba48_2/index.html",
-    "files/webarchive/05012024_227abb8/index.html",
-    "files/webarchive/07202024_13cab6e_1/index.html",
-    "files/webarchive/07202024_cd33c7e_2/index.html",
-    "files/webarchive/07202024_021e208_3/index.html",
-    "files/webarchive/07202024_2d944bd_4/index.html",
-    "files/webarchive/07232024_9eeaa41/index.html",
+    "../04092024_30b04de_1/index.html",
+    "../04092024_ef4a586_2/index.html",
+    "../04102024_12c236c_1/index.html",
+    "../04102024_374ba48_2/index.html",
+    "../05012024_227abb8/index.html",
+    "../07202024_13cab6e_1/index.html",
+    "../07202024_cd33c7e_2/index.html",
+    "../07202024_021e208_3/index.html",
+    "../07202024_2d944bd_4/index.html",
     "index.html"
 ]
 
